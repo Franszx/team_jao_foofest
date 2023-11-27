2cfd3b269cedc62f8bafd1df569d705aa66fd566
Oliver G.A. <hi@oliverga.com>
2023-11-28 00:05:21 +0100

    🔧 chore(page.js): add AOS library and initialize it in useEffect hook to enable animations on the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button class="btn w-64 rounded-full">Button</button>
