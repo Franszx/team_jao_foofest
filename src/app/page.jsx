d750a9f04673b81043370b2bd4ded5a5ef43ec2b
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-02 14:33:40 +0100

    burger menu up

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import BurgerMenu from "@/components/BurgerMenu";
 
 export default function Home() {
   useEffect(() => {
@@ -15,6 +16,8 @@ export default function Home() {
   return (
     <>
       <Header />
+      <BurgerMenu />
+
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <button class="btn w-64 rounded-full">Button</button>
       </main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,43 @@
 import Link from "next/link";
-import { useState } from "react";
-import { slide as Menu } from "react-burger-menu";
-import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
+
 import Image from "next/image";
 
-function BurgerIcon({ isOpen, onClick }) {
-  return (
-    <div className={`relative w-6 h-6 ${isOpen ? "open" : ""}`} onClick={onClick}>
-      <div className="absolute h-0.5 w-full bg-white rounded-full transition-transform duration-200 ease-in-out transform origin-center" style={{ top: "33%", left: "50%", transform: `translate(-60%, -25%) rotate(${isOpen ? "45deg" : "0"})` }}></div>
-
-      <div className="absolute h-0.5 w-full bg-white rounded-full transition-transform duration-200 ease-in-out transform origin-center" style={{ bottom: "33%", left: "50%", transform: `translate(-60%, -25%) rotate(${isOpen ? "-45deg" : "0"})` }}></div>
-    </div>
-  );
-}
-
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(false);
+
+  useEffect(() => {
+    const isDesktopOrLaptopQuery = window.matchMedia("(min-width: 1224px)");
+    setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
+    const listener = () => setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
+    isDesktopOrLaptopQuery.addListener(listener);
+    return () => isDesktopOrLaptopQuery.removeListener(listener);
+  }, []);
 
   return (
-    <header className="flex justify-center items-center py-4 ">
-      <div className="flex justify-between items-center w-full max-w-5xl px-4">
+    <header className="flex justify-center items-center py-4 px-4">
+      <div className="flex justify-between items-center w-full max-w-5xl">
         <div>
-          <Image src="/foofest-logo.svg" alt="logo" height="300" width="100" />
+          <Image className="w-32 lg:w-82" src="./foofest-logo.svg" height="200" width="200" alt="logo" />
         </div>
-        <div className="flex items-center text-white">
-          {isMobile ? (
-            <Menu className="mt-10" right isOpen={isOpen} onStateChange={({ isOpen }) => setIsOpen(isOpen)} customBurgerIcon={<BurgerIcon isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />} customCrossIcon={false}>
-              <Link onClick={() => setIsOpen(false)} href="/">
-                Schedule
-              </Link>
-              <Link onClick={() => setIsOpen(false)} href="/">
-                Stages
-              </Link>
-              <Link onClick={() => setIsOpen(false)} href="/">
-                About FooFest
-              </Link>
-            </Menu>
-          ) : (
-            <nav className="flex ml-4">
-              <ul className="flex space-x-6">
-                <li>
-                  <Link href="/">Schedule</Link>
-                </li>
-                <li>
-                  <Link href="/">Stages</Link>
-                </li>
-                <li>
-                  <Link href="/">About FooFest</Link>
-                </li>
-              </ul>
-            </nav>
-          )}
-          <button className="ml-4 bg-primary text-white px-4 py-2 rounded ">Køb billeter</button>
-        </div>
+
+        {isDesktopOrLaptop && (
+          <nav className="flex ml-4">
+            <ul className="flex space-x-6">
+              <li>
+                <Link href="/">Schedule</Link>
+              </li>
+              <li>
+                <Link href="/">Stages</Link>
+              </li>
+              <li>
+                <Link href="/">About FooFest</Link>
+              </li>
+            </ul>
+          </nav>
+        )}
       </div>
+      <button className="ml-4 bg-primary text-white px-8 py-2 rounded text-xs">Køb billeter</button>
     </header>
   );
 }
