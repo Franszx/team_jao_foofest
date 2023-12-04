b8f7858ee6b6ae7429f263d3623eb592b5c29109
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-04 14:19:54 +0100

    update-heroIsReady

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="foofest">
-      <body className=" bg-gray-950 text-gray-50">{children}</body>
+      <body className="bg-gray-950 text-gray-50">{children}</body>
     </html>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,27 +3,30 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Hero() {
-  const [pages, setPages] = useState([]);
+  const [pages1, setPages1] = useState([]);
+  const [pages2, setPages2] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("http://localhost:8080/bands/");
       const data = await res.json();
-      const bands = data.slice(0, 10);
+      const bands1 = data.slice(0, 10);
+      const bands2 = data.slice(11, 30);
 
-      setPages(bands);
+      setPages1(bands1);
+      setPages2(bands2);
     };
 
     fetchData();
   }, []);
   return (
-    <div className="hero flex flex-col bg-gray-900 mt-10 lg:mt-32">
-      <div className="hero-content">
-        <div className="grid ">
-          <h2 className="grid gap-2 auto-rows-auto text-3xl lg:text-6xl text-stroke-1 font-bold text-transparent ">
-            {pages.map((band) => {
+    <div className="hero flex flex-col lg:mt-10 lg:mt-32">
+      <div className="hero-content text-center">
+        <div>
+          <h2 className="flex flex-wrap font-sans justify-center gap-4 text-4xl lg:text-6xl w-fit font-extrabold text-transparent tracking-tight lg:tracking-tighter lg:leading-58 ">
+            {pages1.map((band) => {
               return (
-                <Link href={`/artist/${band.slug}`} key={band.id}>
+                <Link href={`/artist/${band.slug}`} key={band.id} className="hover:text-gray-300 text-stroke-1">
                   {band.name}
                 </Link>
               );
@@ -31,13 +34,13 @@ export default function Hero() {
           </h2>
         </div>
       </div>
-      <div className="">
+      <div className="hero-content px-2 mt-2">
         <nav className="">
           <ul className="flex grow">
-            <li className="grid text-base font-light">
-              {pages.map((band) => {
+            <li className="flex flex-wrap font-sans text-gray-300 justify-center gap-2 font-semibold text-xs lg:text-lg lg:text-xl font-light ">
+              {pages2.map((band) => {
                 return (
-                  <Link href="/" key={band.id}>
+                  <Link href={`/artist/${band.slug}`} key={band.id}>
                     {band.name}
                   </Link>
                 );
