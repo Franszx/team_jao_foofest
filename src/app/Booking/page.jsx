f12e229e4a3c6c1ef67df208b218db827a516dc7
Oliver G.A. <hi@oliverga.com>
2023-12-05 10:34:25 +0100

    🐛 fix(page.jsx): fix missing dependencies in useEffect hooks to prevent stale data and potential bugs ✨ feat(page.jsx): add functionality to select a camp spot and update choices accordingly 🔧 chore(page.jsx): fetch available spots from server and update every 30 seconds to keep data up to date 🎨 style(page.jsx): improve UI by updating text and button labels for better user experience

--- src/app/Booking/page.jsx
+++ src/app/Booking/page.jsx
@@ -9,8 +9,10 @@ function Booking() {
   const [vipTickets, setVipTickets] = useState(0);
   const [totalTickets, setTotalTickets] = useState(0);
   const [totalPrice, setTotalPrice] = useState(99);
-
   const [allChoices, setAllChoices] = useState({});
+
+  const [spots, setSpots] = useState([]);
+  const [selectedSpot, setSelectedSpot] = useState(null);
 
   const updateTickets = (type, operation) => {
     const isVip = type === "vip";
@@ -25,8 +27,14 @@ function Booking() {
         operation === "increment" ? currentTickets + 1 : currentTickets - 1;
       const setTickets = isVip ? setVipTickets : setRegularTickets;
       setTickets(newTickets);
-      setAllChoices({ ...allChoices, [type + "Tickets"]: newTickets });
-      setTotalTickets(totalTickets + (operation === "increment" ? 1 : -1));
+      const newTotalTickets =
+        totalTickets + (operation === "increment" ? 1 : -1);
+      setTotalTickets(newTotalTickets);
+      setAllChoices({
+        ...allChoices,
+        [type + "Tickets"]: newTickets,
+        totalTickets: newTotalTickets,
+      });
       if (totalPrice > 99 || operation === "increment") {
         setTotalPrice(
           totalPrice + (operation === "increment" ? price : -price)
@@ -43,12 +51,48 @@ function Booking() {
     }
   }
 
+  function selectSpot(spot) {
+    setSelectedSpot(spot);
+    setAllChoices({ ...allChoices, spot });
+  }
+
+  useEffect(() => {
+    const selectedSpotDetails = spots.find(
+      (spot) => spot.area === selectedSpot
+    );
+    if (
+      (selectedSpotDetails && totalTickets > selectedSpotDetails.available) ||
+      totalTickets === 0
+    ) {
+      setSelectedSpot(null);
+      setAllChoices((prevChoices) => {
+        const newChoices = { ...prevChoices };
+        delete newChoices.spot;
+        return newChoices;
+      });
+    }
+  }, [totalTickets, selectedSpot, spots]);
+
   useEffect(() => {
     console.log(allChoices);
   }, [allChoices]);
 
   useEffect(() => {
     AOS.init();
+  }, []);
+
+  useEffect(() => {
+    const fetchSpots = () => {
+      fetch("http://localhost:8080/available-spots")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setSpots(data);
+        });
+    };
+    fetchSpots();
+    const interval = setInterval(fetchSpots, 30000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -58,7 +102,7 @@ function Booking() {
         data-aos="fade-up"
       >
         <div className="bg-gray-900 w-full md:w-7/12 h-full order-2 md:order-1 p-6 md:p-12 flex flex-col justify-between">
-          <h1 className="font-medium text-lg">Choose Tickets and Camp</h1>
+          <h1 className="font-medium text-lg">Tickets & Camp</h1>
           <div className="place-self-center flex flex-col gap-4">
             <div className="space-y-12">
               <div className="flex items-center gap-6 w-60 justify-end">
@@ -167,6 +211,33 @@ function Booking() {
               <p>{totalTickets}</p>
             </div>
           </div>
+          <div>
+            <ul className="flex flex-wrap items-center justify-around gap-4">
+              {spots.map((spot) => (
+                <li key={spot.area}>
+                  <div className="flex flex-col justify-start gap-1">
+                    <button
+                      className={`btn ${
+                        totalTickets > 0 && totalTickets <= spot.available
+                          ? selectedSpot === spot.area
+                            ? "bg-gray-500 text-gray-50 border-gray-400 hover:bg-gray-500 hover:border-gray-400"
+                            : "bg-neutral text-gray-50 border-gray-600 hover:bg-gray-600 hover:border-gray-500"
+                          : "btn-disabled"
+                      } font-medium text-base rounded py-1 px-4 w-fit border  `}
+                      onClick={() => selectSpot(spot.area)}
+                    >
+                      {spot.area}
+                    </button>
+                    <p className="text-gray-400 text-sm font-medium">
+                      {spot.available === 1
+                        ? "1 Spot"
+                        : `${spot.available} Spots`}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="place-self-end space-x-6">
             <button
               className="text-gray-500 hover:text-gray-400 transition-colors font-medium"
@@ -176,13 +247,17 @@ function Booking() {
             </button>
             <button
               className={`btn ${
-                totalTickets > 0
+                totalTickets > 0 && selectedSpot
                   ? "bg-primary text-emerald-100"
                   : "btn-disabled"
               } font-medium text-base rounded py-1 px-4 w-fit border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400 `}
-              onClick={() => changeSlide("next")}
+              onClick={() => {
+                if (totalTickets > 0 && selectedSpot) {
+                  changeSlide("next");
+                }
+              }}
             >
-              {totalTickets > 0 ? "Continue" : "Select Tickets"}
+              Continue
             </button>
           </div>
         </div>
@@ -241,7 +316,10 @@ function Booking() {
               <div className="flex flex-col">
                 <p className="text-gray-400">Regular Tickets</p>
                 <p className="text-gray-50">
-                  <span>799 DKK * {regularTickets}</span>
+                  <span>
+                    799 DKK{" "}
+                    <span className="text-gray-400">* {regularTickets}</span>
+                  </span>
                 </p>
               </div>
             </div>
@@ -270,7 +348,37 @@ function Booking() {
               <div className="flex flex-col">
                 <p className="text-gray-400">VIP Tickets</p>
                 <p className="text-gray-50">
-                  <span>799 DKK * {vipTickets}</span>
+                  <span>
+                    1299 DKK{" "}
+                    <span className="text-gray-400">* {vipTickets}</span>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="rounded-lg bg-gray-600 border border-gray-500 p-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-tent"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M11 14l4 6h6l-9 -16l-9 16h6l4 -6" />
+                </svg>
+              </div>
+              <div className="flex flex-col">
+                <p className="text-gray-400">Selected Camp</p>
+                <p className="text-gray-50">
+                  <span>
+                    {selectedSpot ? selectedSpot : "No camp selected"}
+                  </span>
                 </p>
               </div>
             </div>
