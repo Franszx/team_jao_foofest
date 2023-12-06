a0c2b67e3af238074e9b3ff6eaf96f2ac876d828
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-06 09:59:52 +0100

    button update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,52 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-
+import BuyButton from "./BuyButton";
 import Image from "next/image";
 
-function Header() {
-	const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(false);
+export default function Header() {
+  const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(false);
 
-	useEffect(() => {
-		const isDesktopOrLaptopQuery = window.matchMedia("(min-width: 1224px)");
-		setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
-		const listener = () => setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
-		isDesktopOrLaptopQuery.addListener(listener);
-		return () => isDesktopOrLaptopQuery.removeListener(listener);
-	}, []);
+  useEffect(() => {
+    const isDesktopOrLaptopQuery = window.matchMedia("(min-width: 1224px)");
+    setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
+    const listener = () => setIsDesktopOrLaptop(isDesktopOrLaptopQuery.matches);
+    isDesktopOrLaptopQuery.addListener(listener);
+    return () => isDesktopOrLaptopQuery.removeListener(listener);
+  }, []);
 
-	return (
-		<header className="flex justify-center items-center py-4 px-4">
-			<div className="flex justify-between items-center w-full max-w-5xl">
-				<div>
-					<Image
-						className="w-32 lg:w-82"
-						src="./foofest-logo.svg"
-						height="200"
-						width="200"
-						alt="logo"
-					/>
-				</div>
+  return (
+    <header className="container mx-auto max-w-6xl px-6">
+      <div className="flex justify-center items-center py-4 px-4 mt-8 lg:mt-16 ">
+        <div className="flex justify-between items-center w-full max-w-5xl">
+          <div className="w-32 lg:w-60">
+            <Image src="./foofest-logo.svg" height="500" width="500" alt="logo" />
+          </div>
 
-				{isDesktopOrLaptop && (
-					<nav className="flex ml-4">
-						<ul className="flex space-x-6">
-							<li>
-								<Link href="/">Schedule</Link>
-							</li>
-							<li>
-								<Link href="/">Stages</Link>
-							</li>
-							<li>
-								<Link href="/">About FooFest</Link>
-							</li>
-						</ul>
-					</nav>
-				)}
-			</div>
-			<button className="ml-4 bg-primary text-white px-8 py-2 rounded text-xs">
-				Køb billeter
-			</button>
-		</header>
-	);
+          {isDesktopOrLaptop && (
+            <nav className="flex ml-4">
+              <ul className="flex space-x-6">
+                <li>
+                  <Link className="font-extralight" href="/schedule">
+                    Schedule
+                  </Link>
+                </li>
+                <li>
+                  <Link className="font-extralight" href="/">
+                    Stages
+                  </Link>
+                </li>
+                <li>
+                  <Link className="font-extralight" href="/">
+                    About FF
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+        <BuyButton />
+      </div>
+    </header>
+  );
 }
-
-export default Header;
