9556895ff88a10285bad41d2e2069274316c0abd
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-07 11:01:12 +0100

    nr 2 bla

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,12 +1,10 @@
-"use client";
-
 import Header from "@/components/Header";
 
-export default function Schedule() {
-  <>
-    <Header />
-    <div>
-      <h2>Schedule</h2>
-    </div>
-  </>;
+export default async function Schedule() {
+  return (
+    <>
+      <Header />
+      <div></div>
+    </>
+  );
 }
