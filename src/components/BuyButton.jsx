ac0ca79376c154c801442220e525405ecd4d2b29
Oliver G.A. <hi@oliverga.com>
2023-12-07 10:49:02 +0100

    🎨 style(BuyButton.jsx): improve styling of BuyButton component for better readability and consistency

--- src/components/BuyButton.jsx
+++ src/components/BuyButton.jsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Link href="/booking" legacyBehavior>
-      <a id="link" className="btn ml-4 px-8 py-2 bg-primary text-emerald-100 text-xs lg:text-base w-fit rounded border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400">
+      <a
+        id="link"
+        className="btn bg-primary text-emerald-100 font-medium text-base rounded py-1 px-4 w-fit border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400"
+      >
         Buy Tickets
       </a>
     </Link>
