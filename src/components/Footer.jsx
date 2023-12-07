ad626390425e48dffdda3fbf011c188781ede046
Oliver G.A. <hi@oliverga.com>
2023-12-07 10:37:16 +0100

    🎨 style(Footer.jsx): update margin and padding in footer component for better spacing and alignment 🔗 chore(Footer.jsx): update link formatting in footer component for better readability 🌈 chore(Footer.jsx): update social media icons in footer component for better styling and consistency 🔧 chore(Footer.jsx): update email address in footer component for better contact information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { SocialIcon } from "react-social-icons";
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="container mx-auto max-w-6xl">
-      <div className="hero flex flex-col my-10">
+    <footer className="container mx-auto max-w-6xl mt-20">
+      <div className="hero flex flex-col mb-10">
         <div className="hero-content flex flex-wrap mb-4">
           <ul className="grid grid-cols-2 lg:grid-cols-4 gap-x-9 lg:gap-x-36 justify-items-center ">
             <li>
@@ -12,7 +12,10 @@ export default function Footer() {
               </a>
             </li>
             <li>
-              <Link className="text-sm lg:text-sm text-gray-300" href="/booking">
+              <Link
+                className="text-sm lg:text-sm text-gray-300"
+                href="/booking"
+              >
                 Tickets
               </Link>
             </li>
@@ -49,10 +52,30 @@ export default function Footer() {
           </ul>
         </div>
         <div className="hero-content flex flex-wrap mb-4">
-          <SocialIcon network="facebook" bgColor="#030712" fgColor="#A1A1AA" url="www.facebook.com" />
-          <SocialIcon network="x" bgColor="#030712" fgColor="#A1A1AA" url="www.twitterx.com" />
-          <SocialIcon network="instagram" bgColor="#030712" fgColor="#A1A1AA" url="www.instagram.com" />
-          <SocialIcon network="snapchat" bgColor="#030712" fgColor="#A1A1AA" url="www.snapchat.com" />
+          <SocialIcon
+            network="facebook"
+            bgColor="#030712"
+            fgColor="#A1A1AA"
+            url="www.facebook.com"
+          />
+          <SocialIcon
+            network="x"
+            bgColor="#030712"
+            fgColor="#A1A1AA"
+            url="www.twitterx.com"
+          />
+          <SocialIcon
+            network="instagram"
+            bgColor="#030712"
+            fgColor="#A1A1AA"
+            url="www.instagram.com"
+          />
+          <SocialIcon
+            network="snapchat"
+            bgColor="#030712"
+            fgColor="#A1A1AA"
+            url="www.snapchat.com"
+          />
         </div>
         <div>
           <p className="text-gray-300">info@foofest.com</p>
