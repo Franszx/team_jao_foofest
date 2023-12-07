402fdb299fef2ff27f1713c2f0867a3395b2c2a6
jaco266w <jaco266w@stud.kea.dk>
2023-12-07 21:16:41 +0100

    stage chat page setup plus correct link hrefs

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -3,40 +3,64 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export default function BurgerMenu() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth <= 768);
-    checkMobile();
-    window.addEventListener("resize", checkMobile);
-    return () => window.removeEventListener("resize", checkMobile);
-  }, []);
+	useEffect(() => {
+		const checkMobile = () => setIsMobile(window.innerWidth <= 768);
+		checkMobile();
+		window.addEventListener("resize", checkMobile);
+		return () => window.removeEventListener("resize", checkMobile);
+	}, []);
 
-  return (
-    <div className="fixed z-50 left-2/3 top-3/4">
-      <div>
-        {isMobile && (
-          <div className=" dropdown dropdown-left">
-            <div tabIndex={0} role="button" className="btn m-1 bg-transparent border-none" onClick={() => setIsOpen(!isOpen)}>
-              {isOpen ? <Image className="w-32" src="./cross.svg" height="200" width="200" alt="logo" /> : <Image className="w-32" src="./burger.svg" height="200" width="200" alt="logo" />}
-            </div>
-            {isOpen && (
-              <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-neutral rounded-box w-52">
-                <li>
-                  <a href="/schedule">Schedule</a>
-                </li>
-                <li>
-                  <a>Stages</a>
-                </li>
-                <li>
-                  <a>About FF</a>
-                </li>
-              </ul>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="fixed z-50 left-2/3 top-3/4">
+			<div>
+				{isMobile && (
+					<div className=" dropdown dropdown-left">
+						<div
+							tabIndex={0}
+							role="button"
+							className="btn m-1 bg-transparent border-none"
+							onClick={() => setIsOpen(!isOpen)}
+						>
+							{isOpen ? (
+								<Image
+									className="w-32"
+									src="./cross.svg"
+									height="200"
+									width="200"
+									alt="logo"
+								/>
+							) : (
+								<Image
+									className="w-32"
+									src="./burger.svg"
+									height="200"
+									width="200"
+									alt="logo"
+								/>
+							)}
+						</div>
+						{isOpen && (
+							<ul
+								tabIndex={0}
+								className="dropdown-content z-[1] menu shadow bg-neutral rounded-box w-52"
+							>
+								<li>
+									<a href="/schedule">Schedule</a>
+								</li>
+								<li>
+									<a href="/stageChat">Stage Chat</a>
+								</li>
+								<li>
+									<a>About FF</a>
+								</li>
+							</ul>
+						)}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
