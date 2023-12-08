ffd8e2efe8063fe60de842a155423a4b4aa4f796
jaco266w <jaco266w@stud.kea.dk>
2023-12-08 10:05:00 +0100

    livechat as component

---
+++ src/components/LiveChat.jsx
@@ -0,0 +1,103 @@
+"use client";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from "uuid";
+
+const supabase = createClient(
+	"https://urhncfuwsqbvnyotdqmh.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyaG5jZnV3c3Fidm55b3RkcW1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5ODAzOTIsImV4cCI6MjAxNzU1NjM5Mn0.p3JncVVNOfKr1dMrWFcYxXKxHesdYaRlwIwZs7hqd_Y"
+);
+
+let deviceID = localStorage.getItem("deviceID");
+
+if (!deviceID) {
+	deviceID = uuidv4();
+	localStorage.setItem("deviceID", deviceID);
+}
+
+console.log(deviceID);
+// 07ac6188-d428-41e4-a8a0-22b58267596f
+
+export default function LiveChat(props) {
+	const [messages, setMessages] = useState([]);
+	const [newMessage, setNewMessage] = useState("");
+
+	const handleInserts = (payload) => {
+		setMessages((messages) => [...messages, payload.new]);
+	};
+
+	const handleNewMessageChange = (event) => {
+		setNewMessage(event.target.value);
+	};
+
+	const handleNewMessage = async (event) => {
+		event.preventDefault();
+		console.log(newMessage);
+		const { data, error } = await supabase
+			.from(props.tableName)
+			.insert([{ message: newMessage, sender: deviceID }]);
+		if (error) {
+			console.error("Error inserting:", error);
+		} else {
+			setNewMessage("");
+		}
+	};
+
+	useEffect(() => {
+		const fetchMessages = async () => {
+			const { data, error } = await supabase.from(props.tableName).select("*");
+			if (error) {
+				console.error("Error fetching messages:", error);
+			} else {
+				setMessages(data);
+			}
+		};
+
+		fetchMessages();
+	}, [props.tableName]);
+
+	useEffect(() => {
+		const subscription = supabase
+			.channel("todos")
+			.on(
+				"postgres_changes",
+				{ event: "*", schema: "public", table: props.tableName },
+				handleInserts
+			)
+			.subscribe();
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	}, [props.tableName]);
+
+	// Rest of your component
+	return (
+		<div>
+			<h1>Chat</h1>
+			<form onSubmit={handleNewMessage}>
+				<input
+					type="text"
+					className="bg-gray-900"
+					value={newMessage}
+					onChange={handleNewMessageChange}
+				/>
+				<button type="submit">Send</button>
+			</form>
+			<div className="chat chat-end">
+				{messages.map((message, index) => (
+					<div
+						key={index}
+						className={
+							message.sender === deviceID
+								? "chat-bubble mt-4 chat-bubble-primary"
+								: "chat-bubble mt-4 chat-bubble-accent"
+						}
+					>
+						{message.message}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
