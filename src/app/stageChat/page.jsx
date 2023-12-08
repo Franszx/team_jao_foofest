3c2729c044cf9857f6afd6cc927dcd4d9c259a37
jaco266w <jaco266w@stud.kea.dk>
2023-12-08 13:15:16 +0100

    livechat last styling max-w text

--- src/app/stageChat/page.jsx
+++ src/app/stageChat/page.jsx
@@ -8,14 +8,17 @@ export default function page() {
 		<>
 			<Header />
 			<BurgerMenu />
-			<main className="container mx-auto max-w-6xl px-6 mt-40 flex flex-col gap-8">
-				<h1 className="text-3xl capitalize">
-					Welcome To Our <span className="text-primary">stage chats</span>
-				</h1>
-				<p className="text-lg">
-					We have three stage chats, one for each of our stages. Feel free to
-					chat with other attendees, or ask questions to the speakers!
-				</p>
+			<main className="container mx-auto max-w-6xl px-6 mt-40 flex flex-col items-center gap-8">
+				<div className="max-w-[600px]">
+					<h1 className="text-3xl capitalize mb-4">
+						Welcome To Our{" "}
+						<span className="text-primary uppercase">stage chats</span>
+					</h1>
+					<p className="text-lg">
+						We have three stage chats, one for each of our stages. Feel free to
+						chat with other attendees, or ask questions to the speakers!
+					</p>
+				</div>
 				<WrapperLiveChat />
 			</main>
 			<Footer />
