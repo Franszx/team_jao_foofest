77588c2a6a3af61465816ce6928958c47a635187
jaco266w <jaco266w@stud.kea.dk>
2023-12-11 10:30:07 +0100

    new variable in env.local {url}

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Manual override for testing, comment out when done:
-// export const url = "http://localhost:8080";
-
 // Deployment server:
-export const url = "http://foofest.glitch.me";
+// export const url = "http://foofest.glitch.me";
+export const url = process.env.NEXT_PUBLIC_BACKEND_URL
