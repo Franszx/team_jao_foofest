9c9034eb62dd12de7a07374b9b26af0ade010abb
jaco266w <jaco266w@stud.kea.dk>
2023-12-11 11:04:16 +0100

    buybutton updated link

--- src/components/BuyButton.jsx
+++ src/components/BuyButton.jsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 export default function Header() {
-  return (
-    <Link href="/booking" legacyBehavior>
-      <a
-        id="link"
-        className="btn bg-primary text-emerald-100 font-medium text-base rounded py-1 px-4 w-fit border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400"
-      >
-        Buy Tickets
-      </a>
-    </Link>
-  );
+	return (
+		<Link
+			id="link"
+			href="/booking"
+			className="btn bg-primary text-emerald-100 font-medium text-base rounded py-1 px-4 w-fit border border-emerald-500 hover:bg-emerald-500 hover:border-emerald-400"
+		>
+			Buy Tickets
+		</Link>
+	);
 }
