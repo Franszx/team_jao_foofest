7e571eed6bc3db8841593aeaae9c1398f510ef3e
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-11 12:55:24 +0100

    schedule is merged

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Deployment server:
 // export const url = "https://foofest.glitch.me";
-export const url = process.env.NEXT_PUBLIC_BACKEND_URL
+export const url = process.env.NEXT_PUBLIC_BACKEND_URL;
