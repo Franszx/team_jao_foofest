699a0c395110c8ef5088da6f271ba7da23a9c63c
jaco266w <jaco266w@stud.kea.dk>
2023-12-12 14:33:26 +0100

    loading animation on hero and campoverview

--- src/components/CampOverview.jsx
+++ src/components/CampOverview.jsx
@@ -25,37 +25,40 @@ export default function CampOverview() {
 			<h2 className="text-gray-600 mb-4">
 				<span className="text-xl text-gray-50">Camp Map</span> & Avaliable Spots
 			</h2>
-			<div className="md:grid md:grid-cols-3 md:gap-6 md:place-items-center">
-				<div className="hidden md:flex md:flex-col md:items-center md:w-full">
-					{dataCamps &&
-						dataCamps.map((camp) => {
-							return (
-								<CampDisplay
-									key={camp.area}
-									campName={camp.area}
-									available={camp.available}
-									spots={camp.spots}
-									camp={camp}
-									selectedCamp={selectedCamp}
-									onClick={(e) => {
-										setSelectedCamp(camp);
-									}}
-								/>
-							);
-						})}
-					<div className="mt-4">
+			{!dataCamps && <div className="skeleton w-full h-96 md:h-[700px]"></div>}
+			{dataCamps && (
+				<div className="md:grid md:grid-cols-3 md:gap-6 md:place-items-center">
+					<div className="hidden md:flex md:flex-col md:items-center md:w-full">
+						{dataCamps &&
+							dataCamps.map((camp) => {
+								return (
+									<CampDisplay
+										key={camp.area}
+										campName={camp.area}
+										available={camp.available}
+										spots={camp.spots}
+										camp={camp}
+										selectedCamp={selectedCamp}
+										onClick={(e) => {
+											setSelectedCamp(camp);
+										}}
+									/>
+								);
+							})}
+						<div className="mt-4">
+							<BuyButton />
+						</div>
+					</div>
+					<CampMap
+						selectedCamp={selectedCamp}
+						setSelectedCamp={setSelectedCamp}
+						className="aspect-[4/5] w-full md:col-start-2 md:col-end-4 md:aspect-[5/4]"
+					/>
+					<div className="flex justify-center mt-6 md:hidden">
 						<BuyButton />
 					</div>
 				</div>
-				<CampMap
-					selectedCamp={selectedCamp}
-					setSelectedCamp={setSelectedCamp}
-					className="aspect-[4/5] w-full md:col-start-2 md:col-end-4 md:aspect-[5/4]"
-				/>
-				<div className="flex justify-center mt-6 md:hidden">
-					<BuyButton />
-				</div>
-			</div>
+			)}
 		</section>
 	);
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,6 +21,33 @@ export default function Hero() {
 
 		fetchData();
 	}, []);
+
+	if (pages1.length === 0 || pages2.length === 0)
+		return (
+			<div className="flex mx-auto flex-col gap-6 items-center w-80 md:w-96">
+				<span className="loading loading-spinner text-primary w-16"></span>
+				<div role="alert" className="alert shadow-lg">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						className="stroke-info shrink-0 w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+						></path>
+					</svg>
+					<div>
+						<h3 className="font-bold">Starting Database!</h3>
+						<div className="text-xs">Please Stand By</div>
+					</div>
+				</div>
+			</div>
+		);
+
 	return (
 		<div className="hero flex flex-col -mt-12 lg:my-4 ">
 			<div className="hero-content text-center">
