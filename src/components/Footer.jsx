4590474e7fa4f0ef56189afbd2a86c1bc128d143
jaco266w <jaco266w@stud.kea.dk>
2023-12-12 14:36:29 +0100

    footer margin mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,94 @@
 import { SocialIcon } from "react-social-icons";
 import Link from "next/link";
 export default function Footer() {
-  return (
-    <footer className="container mx-auto max-w-6xl mt-40">
-      <div className="hero flex flex-col mb-10">
-        <div className="hero-content flex flex-wrap mb-4">
-          <ul className="grid grid-cols-2 lg:grid-cols-4 gap-x-9 lg:gap-x-36 justify-items-center ">
-            <li>
-              <Link className="text-sm lg:text-sm text-gray-300" href="/booking">
-                Tickets
-              </Link>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                Chat
-              </a>
-            </li>
-            <li>
-              <Link href="/about" className="text-sm lg:text-sm text-gray-300">
-                About FooFest
-              </Link>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                Responsibility
-              </a>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                News
-              </a>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                Press
-              </a>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                Internship
-              </a>
-            </li>
-            <li>
-              <a className="text-sm lg:text-sm text-gray-300" href="/">
-                Non-profit & Donations
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="hero-content flex flex-wrap mb-4">
-          <div className="hover:opacity-75">
-            <SocialIcon network="facebook" bgColor="#030712" fgColor="#A1A1AA" url="www.facebook.com" />
-          </div>
-          <div className="hover:hover:opacity-75">
-            <SocialIcon network="x" bgColor="#030712" fgColor="#A1A1AA" url="www.twitterx.com" />
-          </div>
-          <div className="hover:hover:opacity-75">
-            <SocialIcon network="instagram" bgColor="#030712" fgColor="#A1A1AA" url="www.instagram.com" />
-          </div>
-          <div className="hover:hover:opacity-75">
-            <SocialIcon network="snapchat" bgColor="#030712" fgColor="#A1A1AA" url="www.snapchat.com" />
-          </div>
-        </div>
-        <div>
-          <p className="text-gray-300">info@foofest.com</p>
-        </div>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="container mx-auto max-w-6xl mt-10 md:mt-40">
+			<div className="hero flex flex-col mb-10">
+				<div className="hero-content flex flex-wrap mb-4">
+					<ul className="grid grid-cols-2 lg:grid-cols-4 gap-x-9 lg:gap-x-36 justify-items-center ">
+						<li>
+							<Link
+								className="text-sm lg:text-sm text-gray-300"
+								href="/booking"
+							>
+								Tickets
+							</Link>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								Chat
+							</a>
+						</li>
+						<li>
+							<Link href="/about" className="text-sm lg:text-sm text-gray-300">
+								About FooFest
+							</Link>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								Responsibility
+							</a>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								News
+							</a>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								Press
+							</a>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								Internship
+							</a>
+						</li>
+						<li>
+							<a className="text-sm lg:text-sm text-gray-300" href="/">
+								Non-profit & Donations
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="hero-content flex flex-wrap mb-4">
+					<div className="hover:opacity-75">
+						<SocialIcon
+							network="facebook"
+							bgColor="#030712"
+							fgColor="#A1A1AA"
+							url="www.facebook.com"
+						/>
+					</div>
+					<div className="hover:hover:opacity-75">
+						<SocialIcon
+							network="x"
+							bgColor="#030712"
+							fgColor="#A1A1AA"
+							url="www.twitterx.com"
+						/>
+					</div>
+					<div className="hover:hover:opacity-75">
+						<SocialIcon
+							network="instagram"
+							bgColor="#030712"
+							fgColor="#A1A1AA"
+							url="www.instagram.com"
+						/>
+					</div>
+					<div className="hover:hover:opacity-75">
+						<SocialIcon
+							network="snapchat"
+							bgColor="#030712"
+							fgColor="#A1A1AA"
+							url="www.snapchat.com"
+						/>
+					</div>
+				</div>
+				<div>
+					<p className="text-gray-300">info@foofest.com</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
