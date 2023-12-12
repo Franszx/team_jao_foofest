372d58afc99c93288944898cc97193d4111349d6
Oliver G.A. <hi@oliverga.com>
2023-12-12 09:37:19 +0100

    header gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,41 +4,41 @@ import BuyButton from "./BuyButton";
 import Image from "next/image";
 
 export default function Header() {
-	return (
-		<header className=" mx-auto px-6 py-12 absolute top-0 z-50 w-screen">
-			<div className="flex justify-between items-center w-full max-w-7xl  mx-auto ">
-				<div className="w-32 lg:w-60">
-					<Link href="/">
-						<Image
-							src="/foofest-logo.svg"
-							height="500"
-							width="500"
-							alt="logo"
-							className="w-36"
-						/>
-					</Link>
-				</div>
+  return (
+    <header className=" mx-auto px-6 py-12 absolute top-0 z-50 w-screen header">
+      <div className="flex justify-between items-center w-full max-w-7xl  mx-auto ">
+        <div className="w-32 lg:w-60">
+          <Link href="/">
+            <Image
+              src="/foofest-logo.svg"
+              height="500"
+              width="500"
+              alt="logo"
+              className="w-36"
+            />
+          </Link>
+        </div>
 
-				<nav className="hidden lg:flex gap-12">
-					<ul className="flex gap-6 font-medium items-center">
-						<li>
-							<Link href="/schedule">Schedule</Link>
-						</li>
-						<li>
-							<Link href="/stageChat">Stage Chat</Link>
-						</li>
-						<li>
-							<Link href="/about">About FF</Link>
-						</li>
-						<li>
-							<BuyButton />
-						</li>
-					</ul>
-				</nav>
-				<div className="block lg:hidden font-medium">
-					<BuyButton />
-				</div>
-			</div>
-		</header>
-	);
+        <nav className="hidden lg:flex gap-12">
+          <ul className="flex gap-6 font-medium items-center">
+            <li>
+              <Link href="/schedule">Schedule</Link>
+            </li>
+            <li>
+              <Link href="/stageChat">Stage Chat</Link>
+            </li>
+            <li>
+              <Link href="/about">About FF</Link>
+            </li>
+            <li>
+              <BuyButton />
+            </li>
+          </ul>
+        </nav>
+        <div className="block lg:hidden font-medium">
+          <BuyButton />
+        </div>
+      </div>
+    </header>
+  );
 }
