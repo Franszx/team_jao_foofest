463a907d69ae46e290190fbb160e94b181cab80b
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-15 10:04:20 +0100

    z-index på logocredits - z45

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -34,7 +34,7 @@ export default function ScheduleCard({ scene, ...props }) {
           </div>
           {/* Display logo credits if there are any */}
           {props.logoCredits && (
-            <div className="absolute bottom-2 right-12 z-50" data-tip={props.logoCredits}>
+            <div className="absolute bottom-2 right-12 z-45" data-tip={props.logoCredits}>
               <span title={props.logoCredits} className="italic bg-gray-300 ">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
