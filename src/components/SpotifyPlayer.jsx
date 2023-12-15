87c03092f21e0b9484eea1c501722ad93270a52c
jaco266w <jaco266w@stud.kea.dk>
2023-12-15 10:34:24 +0100

    spotifyplayer conditional rendering

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -4,50 +4,50 @@ import { useEffect, useState } from "react";
 import { url } from "/config";
 
 function SpotifyPlayer({ band }) {
-  const [bands, setBands] = useState([]);
-  const [spotifyId, setSpotifyId] = useState("");
+	const [bands, setBands] = useState([]);
+	const [spotifyId, setSpotifyId] = useState("");
 
-  useEffect(() => {
-    const fetchBands = async () => {
-      const response = await fetch(`${url}/bands`);
-      const data = await response.json();
-      setBands(data);
-    };
+	useEffect(() => {
+		const fetchBands = async () => {
+			const response = await fetch(`${url}/bands`);
+			const data = await response.json();
+			setBands(data);
+		};
 
-    fetchBands();
-  }, []);
+		fetchBands();
+	}, []);
 
-  useEffect(() => {
-    const getSpotifyId = () => {
-      const bandMatch = bands.find((newBand) => newBand.slug === band.slug);
-      if (bandMatch) {
-        setSpotifyId(bandMatch.id);
-      }
-    };
+	useEffect(() => {
+		const getSpotifyId = () => {
+			const bandMatch = bands.find((newBand) => newBand.slug === band.slug);
+			if (bandMatch) {
+				setSpotifyId(bandMatch.id);
+			}
+		};
 
-    if (bands.length > 0) {
-      getSpotifyId();
-    }
-  }, [bands, band]);
+		if (bands.length > 0) {
+			getSpotifyId();
+		}
+	}, [bands, band]);
 
-  if (!band.id) {
-    return null;
-  }
+	if (!spotifyId) {
+		return null;
+	}
 
-  return (
-    <div className="container mx-auto max-w-4xl px-6 mt-6 md:mt-12 flex items-center justify-center min-h-[352px]">
-      {spotifyId ? (
-        <iframe
-          src={`https://open.spotify.com/embed/artist/${spotifyId}?utm_source=generator`}
-          width="100%"
-          height="352"
-          loading="lazy"
-        ></iframe>
-      ) : (
-        <p className="text-gray-400 text-sm">Spotify Player Loading...</p>
-      )}
-    </div>
-  );
+	return (
+		<div className="container mx-auto max-w-4xl px-6 mt-6 md:mt-12 flex items-center justify-center min-h-[352px]">
+			{spotifyId ? (
+				<iframe
+					src={`https://open.spotify.com/embed/artist/${spotifyId}?utm_source=generator`}
+					width="100%"
+					height="352"
+					loading="lazy"
+				></iframe>
+			) : (
+				<p className="text-gray-400 text-sm">Spotify Player Loading...</p>
+			)}
+		</div>
+	);
 }
 
 export default SpotifyPlayer;
