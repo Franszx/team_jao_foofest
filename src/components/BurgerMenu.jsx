c009eca7008d6b8646486b525ffcfb1f3867dd0e
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-15 09:59:22 +0100

    one more burger commit

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -10,7 +10,7 @@ export default function BurgerMenu() {
     <div className="fixed z-50 right-2 bottom-10 md:hidden">
       <div className="dropdown dropdown-left">
         <div tabIndex={0} role="button" className="btn m-1 bg-transparent border-none" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <Image className="w-24" src="/cross.svg" height="100" width="100" alt="logo" /> : <Image className="w-24" src="/greenBurger.svg" height="100" width="100" alt="logo" />}
+          {isOpen ? <Image className="w-20" src="/cross.svg" height="100" width="100" alt="logo" /> : <Image className="w-20" src="/greenBurger.svg" height="100" width="100" alt="logo" />}
         </div>
         {isOpen && (
           <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-neutral rounded-box w-52">
