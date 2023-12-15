a2b31c89d7cd446f3b9e1ee1d0c0a08798af370a
Anders Lindberg <anderslindberg999@gmail.com>
2023-12-15 09:45:17 +0100

    burger bottom-10

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -7,7 +7,7 @@ export default function BurgerMenu() {
   const [isMobile, setIsMobile] = useState(false);
 
   return (
-    <div className="fixed z-50 right-2 bottom-2 md:hidden">
+    <div className="fixed z-50 right-2 bottom-10 md:hidden">
       <div className="dropdown dropdown-left">
         <div tabIndex={0} role="button" className="btn m-1 bg-transparent border-none" onClick={() => setIsOpen(!isOpen)}>
           {isOpen ? <Image className="w-24" src="/cross.svg" height="100" width="100" alt="logo" /> : <Image className="w-24" src="/greenBurger.svg" height="100" width="100" alt="logo" />}
